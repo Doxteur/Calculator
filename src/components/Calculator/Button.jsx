9d2bf9a5a049da09e0c50fe8b74f91fc13393873
Jdoussain <jdoussain@linaia.com>
2023-07-03 10:53:21 +0200

    bug_variable_name_error

--- src/components/Calculator/Button.jsx
+++ src/components/Calculator/Button.jsx
@@ -1,8 +1,8 @@
 const Button = ({ className, value, onClick }) => {
-    return (
-      <button className="w-full h-16 outline-none focus:outline-none hover:bg-indigo-700 hover:bg-opacity-20 text-white text-xl font-light">
+  return (
+    <button className="w-full h-16 outline-none focus:outline-none hover:bg-indigo-700 hover:bg-opacity-20 text-white text-xl font-light" onClick={onClick}>
       {value}
     </button>
-    );
-  };
-  export default Button;
+  );
+};
+export default Button;
--- src/components/Calculator/Calculator.jsx
+++ src/components/Calculator/Calculator.jsx
@@ -1,14 +1,27 @@
-import React from "react";
+import React, { useState,useEffect } from "react";
 import Button from "./Button";
+import {toLocaleString,numClickHandler, signClickHandler, commaClickHandler, equalsClickHandler, percentClickHandler, invertClickHandler, resetClickHandler} from './functions'
 
 function Calculator() {
+  let [calc, setCalc] = useState({
+    sign: "",
+    num: 0,
+    res: 0,
+  });
+
   const btnValues = [
     ["C", "+-", "%", "/"],
     [7, 8, 9, "X"],
     [4, 5, 6, "-"],
     [1, 2, 3, "+"],
-    [0, ".","="],
+    [0, ".", "="],
   ];
+
+  useEffect(() => {
+    console.log(calc);
+  }, [calc]);
+
+  const removeSpaces = (num) => num.toString().replace(/\s/g, "");
 
   return (
     <div>
@@ -18,7 +31,7 @@ function Calculator() {
         >
           <div className="w-full h-40 bg-gradient-to-b from-gray-800 to-gray-700 flex items-end text-right">
             <div className="w-full py-5 px-6 text-6xl text-white font-thin">
-              340.0
+              {toLocaleString(calc.res ? calc.res : calc.num)}  
             </div>
           </div>
           <div className="w-full bg-gradient-to-b from-indigo-400 to-indigo-500">
@@ -30,10 +43,28 @@ function Calculator() {
                   {btn.map((btnCut) => {
                     return (
                       <div className={` border-r border-b border-indigo-400 
-                      ${btnCut == "=" ? 'w-2/4' :'w-1/4'}`}
-                       key={btnCut}>
-                        <Button value={btnCut}  />
-                      
+                      ${btnCut == "=" ? 'w-2/4' : 'w-1/4'}`}
+                        key={btnCut}>
+                        <Button 
+                          value={btnCut}
+                          onClick={(e) => {
+                            console.log(e.target.innerHTML);
+                            btnCut === "C"
+                              ? resetClickHandler(calc,setCalc)
+                              : btnCut === "+-"
+                              ? invertClickHandler(calc,setCalc)
+                              : btnCut === "%"
+                              ? percentClickHandler(calc,setCalc)
+                              : btnCut === "="
+                              ? equalsClickHandler(calc,setCalc)
+                              : btnCut == "/" || btnCut == "X" || btnCut == "-" || btnCut == "+"
+                              ? signClickHandler(e,calc,setCalc)
+                              : btnCut === "."
+                              ? commaClickHandler(e,calc,setCalc)
+                              : numClickHandler(e,calc,setCalc)
+                          }
+                        }
+                        />
                       </div>
                     );
                   })}
